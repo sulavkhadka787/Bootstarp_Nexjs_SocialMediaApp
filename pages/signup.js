81a82b0572a441aec3702e837c58d6d5d7b9990d
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-10-05 21:04:40 -0600

    upload pic preview

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 
 const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
 
@@ -22,11 +24,14 @@ const Signup = () => {
   const { name, email, password, bio } = user;
 
   const [showSocialLinks, setShowSocialLinks] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [userName, setUserName] = useState("");
   const [usernameAvailable, setUsernameAvailable] = useState(false);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
 
   const [media, setMedia] = useState(null);
   const [mediaPreview, setMediaPreview] = useState(null);
+  const [highlighted, setHighlighted] = useState(false);
 
   const [errorMsg, setErrorMsg] = useState(null);
   const [formLoading, setFormLoading] = useState(false);
@@ -44,32 +49,75 @@ const Signup = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+
+  useEffect(() => {
+    const isUser = Object.values({ name, email, password, bio }).every((item) =>
+      Boolean(item)
+    );
+
+    isUser ? setSubmitDisabled(false) : setSubmitDisabled(true);
+  }, [user]);
   return (
     <>
       {JSON.stringify(user)}
       <Row md={2} className="justify-content-md-center">
         <Col>
           <div className="alert alert-success mb-3 mt-3">
-            <i className="fa fa-empire" style={{ fontSize: "36px" }}></i>{" "}
-            <strong>Get Started</strong> <br />
-            Create New Account
+            {errorMsg ? (
+              <strong>{errorMsg}</strong>
+            ) : (
+              <>
+                <i className="fa fa-empire" style={{ fontSize: "36px" }}></i>
+                <strong>Get Started</strong> <br />
+                <span>Create New Account</span>
+              </>
+            )}
           </div>
+          {mediaPreview && (
+            <div className="alert alert-success mt-3 mb-3">
+              <img src={mediaPreview} className="upload-image" />
+            </div>
+          )}
           <Form onSubmit={handleSubmit}>
-            <div className="alert alert-success mb-3 mt-3">
+            <div
+              className="alert alert-success mb-3 mt-3 file-upload"
+              onDragOver={(e) => {
+                e.preventDefault();
+                setHighlighted(true);
+              }}
+              onDragLeave={(e) => {
+                e.preventDefault();
+                setHighlighted(false);
+              }}
+              onDrop={(e) => {
+                e.preventDefault();
+                setHighlighted(true);
+                console.log(e.dataTransfer.files);
+                const droppedFile = Array.from(e.dataTransfer.files);
+                setMedia(droppedFile[0]);
+                setMediaPreview(URL.createObjectURL(droppedFile[0]));
+              }}
+            >
+              Drag or Upload file
+              <br />
               <Form.Control
                 multiple
+                className="mt-3"
                 type="file"
                 name="media"
                 onChange={handleChange}
                 accept="/image/*"
               />
             </div>
+
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control
+                required
                 type="text"
                 placeholder="Enter Name"
                 name="name"
+                value={name}
                 onChange={handleChange}
               />
             </Form.Group>
@@ -77,21 +125,32 @@ const Signup = () => {
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
               <Form.Control
+                required
                 type="email"
                 placeholder="Enter Email"
                 name="email"
+                value={email}
                 onChange={handleChange}
               />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
+
               <Form.Control
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="Password"
                 name="password"
+                value={password}
                 onChange={handleChange}
               />
+
+              <Button
+                className="mt-3 btn btn-secondary"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <i className="fa fa-user-secret"></i> Show Password
+              </Button>
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>UserName</Form.Label>
@@ -177,7 +236,12 @@ const Signup = () => {
               <br />
             )}
 
-            <Button variant="primary" type="submit" className="mb-3">
+            <Button
+              variant="primary"
+              type="submit"
+              className="mb-3"
+              disabled={submitDisabled || !usernameAvailable}
+            >
               Sign-Up
             </Button>
           </Form>
