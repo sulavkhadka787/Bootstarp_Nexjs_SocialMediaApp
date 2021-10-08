73ed46dd054468bf1acf73128172330fd32cc282
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-10-07 20:26:33 -0600

    signup and auth

--- api/signup.js
+++ api/signup.js
@@ -31,4 +31,73 @@ router.get("/:username", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const {
+    name,
+    email,
+    username,
+    password,
+    bio,
+    facebook,
+    youtube,
+    twitter,
+    instagram,
+  } = req.body.user;
+
+  if (!isEmail(email)) return res.status(401).send("Invalid");
+
+  if (password.length < 6) {
+    return res.status(401).send("Password must be atleast 6 characters long");
+  }
+  try {
+    let user;
+    user = await UserModel.findOne({ email: email.toLowerCase() });
+
+    if (user) {
+      return res.status(401).send("User already registered");
+    }
+    user = new UserModel({
+      name,
+      email: email.toLowerCase(),
+      username: username.toLowerCase(),
+      password,
+      profilePicUrl: req.body.profilePicUrl || userPng,
+    });
+
+    user.password = await bcrypt.hash(password, 10);
+    await user.save();
+
+    let profileFields = {};
+    proileFields.user = user._id;
+    profileFields.bio = bio;
+    profileFields.social = {};
+
+    if (facebook) profileFields.social.facebook = facebook;
+    if (youtube) profileFields.social.youtube = youtube;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (twitter) profileFields.social.twitter = twitter;
+
+    await new ProfileModel(profileFields).save();
+    await new FollowerModel({
+      user: user._id,
+      followers: [],
+      following: [],
+    }).save();
+
+    const payload = { userId: user._id };
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      { expiresIn: "2d" },
+      (err, token) => {
+        if (err) throw err;
+        res.status(200).json(token);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
 module.exports = router;
