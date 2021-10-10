5703e0ae3558814f08b1708310b85f784f3ab93f
sulavkhadka787 <sulav.khadka787@gmail.com>
2021-10-09 21:28:15 -0600

    get-initial-props

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,46 @@
 import App from "next/app";
+import axios from "axios";
+import { parseCookies, destroyCookie } from "nookies";
+import baseUrl from "../utils/baseUrl";
 import Layout from "../components/Layout/Layout";
 import "bootstrap/dist/css/bootstrap.css";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const { token } = parseCookies(ctx);
+    let pageProps = {};
+
+    const protectedRoutes = ctx.pathname === "/";
+
+    if (!token) {
+      protectedRoutes && redirectUser(ctx, "/login");
+    } else {
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx);
+      }
+      try {
+        const res = await axios.get(`${baseUrl}/api/auth`, {
+          headers: { Authorization: token },
+        });
+
+        const { user, userFollowStats } = res.data;
+        if (user) !protectedRoutes && redirectUser(ctx, "/");
+        pageProps.user = user;
+        pageProps.userFollowStats = userFollowStats;
+      } catch (error) {
+        destroyCookie(ctx, "token");
+        redirectUser(ctx, "/login");
+      }
+    }
+
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
       <Layout>
-        <Component />
+        <Component {...pageProps} />
       </Layout>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-const Index = () => {
-  return <div>Index page</div>;
+import axios from "axios";
+
+const Index = ({ posts }) => {
+  return (
+    <div>
+      {posts &&
+        posts.length > 0 &&
+        posts.map((post) => <h1 key={post._id}>{post.title}</h1>)}
+    </div>
+  );
+};
+
+Index.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const { name } = ctx.query;
+
+    return { posts: res.data };
+  } catch (error) {
+    return { errorLoading: true };
+  }
 };
 
 export default Index;
